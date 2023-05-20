a52c36ad1978c9b24ed01a72982c5dcb201eff55
Jose Storani <jdstorani91@gmail.com>
2023-05-19 22:27:40 -0300

    flip events alpha

--- src/app.js
+++ src/app.js
@@ -1,43 +1,32 @@
 const overlayText = document.querySelector(".overlay-text");
 const gameOverLay = document.getElementById("gameover-text");
 const restartButton = document.querySelector(".overlay-text-small")
-overlayText.addEventListener("click",()=>{
-    overlayText.classList.remove("visible");
+const timeRemaining = document.getElementById("time-remaining");
+const allCards = document.querySelectorAll(".card");
+const cardsValues = document.querySelectorAll(".card-value")
+const flipCount = document.getElementById("flips")
+
+
+
+
+
+overlayText.addEventListener("click", () => {
+    removeVisibility(overlayText);
     gameStart()
 
 })
 
 
-const gameStart = () => {
-    const timeRemaining = document.getElementById("time-remaining");
-    let count = 5;
-  
-    const interval = setInterval(() => {
-      timeRemaining.innerText = count;
-  
-      gameOver(count,interval)
-  
-      count--;
-    }, 1000);
-  }
-  
-  
-  
-function gameOver(time,intervalID){
-    if(time <= 0){
-        clearInterval(intervalID)
-        setTimeout(() => {
-            gameOverLay.classList.add("visible");
-          }, 0);
-    }
-}
-
-function restart(){
-    gameOverLay.classList.remove("visible");
-    gameStart()
-}
-
-
-restartButton.addEventListener("click",()=>{
+restartButton.addEventListener("click", () => {
     restart()
 })
+
+
+
+
+
+
+
+
+
+
