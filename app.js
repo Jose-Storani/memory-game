a1a0bf9488287fd3b4d6ff0db724bd43f601177e
Jose Storani <jdstorani91@gmail.com>
2023-05-26 12:45:01 -0300

    funcionalidad terminada, falta agregar audio

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ const gameOverLay = document.getElementById("gameover-text");
 const restartButton = document.querySelector(".overlay-text-small")
 const timeRemaining = document.getElementById("time-remaining");
 const allCards = document.querySelectorAll(".card");
+const cardsArray = Array.from(allCards)
 const cardsValues = document.querySelectorAll(".card-value")
 const flipCount = document.getElementById("flips")
 
-
+//obtengo los valores src de cada carta para despues poder mezclar
+let imgValueArray = cardsArray.map((card)=>{
+    const imgCard = card.querySelector(".card-front .card-value");
+    const cardValue = imgCard.getAttribute("src");
+    return cardValue
+})
 
 
 
 overlayText.addEventListener("click", () => {
-    removeVisibility(overlayText);
     gameStart()
+    removeVisibility(overlayText);
+    setGame();
+    
 
 })
 
-
 restartButton.addEventListener("click", () => {
+    setGame()
     restart()
 })
 
---
+++ functions.js
@@ -0,0 +1,122 @@
+let firstCardRevealed = null;
+let revealedCards = [];
+const GAME_DURATION = 99
+let flips = 0;
+
+const gameStart = () => {
+    let count = GAME_DURATION;
+    const interval = setInterval(() => {
+        timeRemaining.innerText = count;
+        gameOver(count, interval);
+        count--;
+    }, 1000);
+    showCard();
+    
+};
+
+function gameOver(time, intervalID) {
+    if (time <= 0) {
+        clearInterval(intervalID);
+        setTimeout(() => {
+            addVisibility(gameOverLay);
+        }, 0);
+    }
+}
+
+function addVisibility(element) {
+    element.classList.add("visible");
+}
+function removeVisibility(element) {
+    element.classList.remove("visible");
+}
+
+function showCard() {
+    cardsArray.forEach((card) => {
+        card.addEventListener("click", () => {
+            addVisibility(card);
+            checkCardValue(card);
+            addFlip();
+        });
+    });
+}
+
+function addFlip() {
+    flips++;
+    flipCount.innerText = flips;
+}
+
+function checkCardValue(element) {
+    revealedCards.push(element);
+    if (revealedCards.length === 2) {
+        clickState();
+    }
+    const imgCard = element.querySelector(".card-front .card-value");
+    const cardValue = imgCard.getAttribute("src");
+
+    if (firstCardRevealed === null) {
+        firstCardRevealed = cardValue;
+    } else {
+        if (firstCardRevealed === cardValue) {
+            clickState("enable");
+            revealedCards = [];
+        } else {
+            setTimeout(() => {
+                revealedCards.forEach((cardChild) => {
+                    const parentCard = cardChild.closest(".card");
+                    removeVisibility(parentCard);
+                });
+                clickState("enable");
+                revealedCards = [];
+            }, 1000);
+        }
+        firstCardRevealed = null;
+    }
+}
+
+function restart() {
+    removeVisibility(gameOverLay);
+    timeRemaining.innerText = 100;
+    flipAllCards();
+    gameStart();
+}
+
+function clickState(state) {
+    if (state === "enable") {
+        cardsArray.forEach((card) => {
+            card.style.pointerEvents = "auto";
+        });
+    } else {
+        cardsArray.forEach((card) => {
+            card.style.pointerEvents = "none";
+        });
+    }
+}
+
+function flipAllCards() {
+    cardsArray.forEach((card) => {
+        removeVisibility(card);
+    });
+}
+
+function shuffleCards(array) {
+    let currentIndex = array.length;
+
+    while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        const tempValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = tempValue;
+    }
+
+    return array;
+}
+
+function setGame() {
+    const shuffledCards = shuffleCards(imgValueArray);
+    cardsArray.forEach((card, index) => {
+        const imgCard = card.querySelector(".card-front .card-value");
+        imgCard.setAttribute("src", shuffledCards[index]);
+    });
+}
